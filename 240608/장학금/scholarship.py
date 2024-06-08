6d86609b5b175824d48eb28a5f953088301bcb6b
ohminsu <95901594+ohminsu@users.noreply.github.com>
2024-06-08 20:45:15 +0900

    Title: 장학금, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240608/장학금/scholarship.py
+++ 240608/장학금/scholarship.py
@@ -1,9 +1,9 @@
 a, b = map(float, input().split())
 
-if 90 <= a and 95 <= a :
+if 90 <= a and 95 <= b :
     print(100000)
 
-elif 90 <= a and 95 > a :
+elif 90 <= a and 90 <= b :
     print(50000)
 
 else :
