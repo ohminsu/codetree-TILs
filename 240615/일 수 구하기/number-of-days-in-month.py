be4eff5d7890feff6e49110ce05a3d20175f4019
ohminsu <95901594+ohminsu@users.noreply.github.com>
2024-06-15 17:39:19 +0900

    Title: 일 수 구하기, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240615/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,42 @@
+a, b, c = 31, 30, 28
+
+n = int(input())
+
+if n == 1 :
+    print(a)
+
+elif n == 2 :
+    print(c)
+
+elif n == 3 :
+    print(a)
+
+elif n == 4 :
+    print(b)
+
+elif n == 5 :
+    print(a)
+
+elif n == 6 :
+    print(b)
+
+elif n == 7 :
+    print(a)
+
+elif n == 8 :
+    print(a)
+
+elif n == 9 :
+    print(b)
+
+elif n == 10 :
+    print(a)
+
+elif n == 11 :
+    print(b)
+
+elif n == 12 :
+    print(a)
+
+else :
+    print()
